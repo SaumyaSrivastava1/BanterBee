196b5ac5d3cf11904056e68ed9a33548870d1e16
Saumya Srivastava <saumya.srivastava.512@gmail.com>
2026-03-05 23:43:22 +0530

    fix cors preflight

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const requestRoutes = require("./routes/requestRoutes"); // <--- ADDED: Request 
 // new
 const app = express();
 
-aapp.use(cors({
+app.use(cors({
   origin: [
     "http://localhost:3000",
     "https://banter-bee.vercel.app"
@@ -23,7 +23,7 @@ aapp.use(cors({
   methods: ["GET","POST","PUT","DELETE"],
   allowedHeaders: ["Content-Type","Authorization"],
 }));
-
+app.options("*", cors());
 app.use(express.json({ limit: "50mb" }));
 
 // --- 1. DEFINE ALLOWED ORIGINS ---
