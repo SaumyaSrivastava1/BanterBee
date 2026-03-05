4a4a0234e4dd0b209774479922428bdd629104b1
Saumya Srivastava <saumya.srivastava.512@gmail.com>
2026-03-05 23:51:07 +0530

    fix cors options route

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(cors({
   methods: ["GET","POST","PUT","DELETE"],
   allowedHeaders: ["Content-Type","Authorization"],
 }));
-app.options("/*", cors());
+
 app.use(express.json({ limit: "50mb" }));
 
 // --- 1. DEFINE ALLOWED ORIGINS ---
